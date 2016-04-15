6b0b94d6f9e281e39b36bd52ff1b473f4ef09d45
crinkelite <crinkelite@yahoo.co.uk>
2016-04-15 03:15:46 +0100

    get names

--- names.py
+++ names.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python
-
 #scrape names to database
-
+import psycopg2
 from bs4 import BeautifulSoup
 
-f = open('names.html', 'r')
+conn = psycopg2.connect("dbname=database user=postgres")
+cur = conn.cursor()
+
+f = open('/home/joe/git/python/names/names.html', 'r')
 data = f.read()
 soup = BeautifulSoup(data, 'lxml')
-num = 1
-for i in soup.find_all('tr'):
-	print(soup.find_all('td')[num])
-	num+=3
-
-
+soup = BeautifulSoup(str(soup.find_all('table')[1]))
+soup = BeautifulSoup(str(soup.find_all('tr')[2:-2]))
+for id, tr in enumerate(soup.find_all('tr')):
+	rank, boy, girl = tr.find_all('td')
+	cur.execute("INSERT INTO test (rank, boy, girl, id) VALUES (%s, %s, %s, %s)",
+		(rank.text, boy.text, girl.text, id))
+conn.commit()
+cur.close()
+conn.close()
